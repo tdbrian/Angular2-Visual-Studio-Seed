35fbf5e0b5c65d91f924fba4ac8e4581e147f93d
Thomas Brian <tbrian@appriver.com>
2016-03-09 10:23:39 -0600

    Update names with Angular2 Visual Studio Seed

--- src/Angular2Test/wwwroot/_references.js
+++ src/Angular2Test/wwwroot/_references.js
@@ -1,5 +1,7 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
+/// <reference path="js/app.component.js" />
+/// <reference path="js/main.js" />
 /// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
